fa9b7b30b9dc3acc7479d78f64837ada672f3b25
unknown <rafazel@outlook.com>
2024-06-20 14:47:23 -0300

    feat: add random conversations mock file

---
+++ src/shared/constants/conversations-random-mock.constant.ts
@@ -0,0 +1,88 @@
+export const CONVERSATIONS_RANDOM_MOCK = [
+  {
+    id: '68e8d068-a4d7-454d-aa65-456640e07786',
+    createdAt: '2022-08-01T00:00:00.000Z',
+    lastMessage: {
+      id: '935f487e-4b61-4e3d-ab64-ce3959efcdfb',
+      content: 'Olá, estou interessada numa tatuagem no braço',
+      createdAt: '2022-08-01T00:00:00.000Z',
+    },
+    profile: {
+      id: 'ab8ccf91-3217-4c99-9a04-4ab656511299',
+      name: 'Larissa Dias',
+      imageProfile:
+        'https://pub-777ce89a8a3641429d92a32c49eac191.r2.dev/profiles%2Fab8ccf91-3217-4c99-9a04-4ab656511299.jpg',
+    },
+  },
+  {
+    id: '68e8d068-a4d7-454d-aa65-456640e07786',
+    createdAt: '2024-08-01T00:00:00.000Z',
+    lastMessage: {
+      id: '935f487e-4b61-4e3d-ab64-ce3959efcdfb',
+      content: 'Ótimo, adorei a proposta, vamos fazer sim!!',
+      createdAt: '2024-08-01T00:00:00.000Z',
+    },
+    profile: {
+      id: 'ab8ccf91-3217-4c99-9a04-4ab656511299',
+      name: 'Larissa Dias',
+      imageProfile:
+        'https://pub-777ce89a8a3641429d92a32c49eac191.r2.dev/profiles%2Fab8ccf91-3217-4c99-9a04-4ab656511299.jpg',
+    },
+  },
+  {
+    id: '2df00b07-72e2-4878-a728-6ff3269ee18d',
+    createdAt: '2021-08-01T00:00:00.000Z',
+    lastMessage: {
+      id: '1b49203a-aa9a-4e8b-953f-41813143039d',
+      content: 'Um símbolo que represente a liberdade',
+      createdAt: '2021-08-01T00:00:00.000Z',
+    },
+    profile: {
+      id: 'd36fd7f7-019f-4fa8-a801-18a3b89ef5fc',
+      name: 'Marcus Freites',
+      imageProfile:
+        'https://pub-777ce89a8a3641429d92a32c49eac191.r2.dev/profiles%2Fd36fd7f7-019f-4fa8-a801-18a3b89ef5fc.jpg',
+    },
+  },
+  {
+    id: 'cce1a8aa-f5e0-49d4-9068-a0272609d957',
+    createdAt: '2020-08-01T00:00:00.000Z',
+    lastMessage: {
+      id: '3a7fb469-22d5-46d8-b720-55c6367d7301',
+      content: 'Olá, bom dia. Queria saber se você faz tatuagem de henna',
+      createdAt: '2020-08-01T00:00:00.000Z',
+    },
+    profile: {
+      id: '442ef9e3-e0f5-4846-b79d-df545cff9d59',
+      name: 'Tatiana Oliveira',
+      imageProfile:
+        'https://pub-777ce89a8a3641429d92a32c49eac191.r2.dev/profiles%2F442ef9e3-e0f5-4846-b79d-df545cff9d59.jpg',
+    },
+  },
+  {
+    id: 'b70b66da-5390-4858-8f4f-a4faabdf91ee',
+    createdAt: '2018-08-01T00:00:00.000Z',
+    lastMessage: {
+      id: 'b3135971-222d-4864-86ff-ec882225f121',
+      content: 'Então, poderia ser para o dia 10 de agosto?',
+      createdAt: '2018-08-01T00:00:00.000Z',
+    },
+    profile: {
+      id: 'af343794-9282-4fcd-ab06-9fdfc3ef41ab',
+      name: 'Diogo Almeida',
+      imageProfile:
+        'https://pub-777ce89a8a3641429d92a32c49eac191.r2.dev/profiles%2Faf343794-9282-4fcd-ab06-9fdfc3ef41ab.jpg',
+    },
+  },
+  {
+    id: '03eca79a-965d-4d73-bb8f-bab1fb4707c5',
+    createdAt: '2023-08-01T00:00:00.000Z',
+    lastMessage: {},
+    profile: {
+      id: '92681908-5505-4739-8044-c3b428e52225',
+      name: 'Andreia Mata',
+      imageProfile:
+        'https://pub-777ce89a8a3641429d92a32c49eac191.r2.dev/profiles%31f74e81-03b4-4d9f-a02b-a5a389e7711c.jpg',
+    },
+  },
+];
